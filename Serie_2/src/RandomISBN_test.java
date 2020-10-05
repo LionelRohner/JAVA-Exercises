6e458214919064e06490167d01e35dfbb6beb62a
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2020-10-05 13:05:14 +0200

    Exercise_2_start

---
+++ Serie_2/src/RandomISBN_test.java
@@ -0,0 +1,54 @@
+/* ************************************************************************* *\
+*                Programmierung 1 HS 2020 - Serie 2 Aufgabe 1                 *
+\* ************************************************************************* */
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class RandomISBN_test
+{
+    // Lionel Rohner (09-123-696)
+
+    public static void main( String args[] )
+    {
+        System.out.println(makeISBN());
+    }
+
+    /** generates and returns a random ISBN in the format XX-XXX-XX-C */
+    public static String makeISBN()
+    {
+        // Do NOT change the declaration of the following variables!
+        String laendercode;
+        String bandnr;
+        String verlagsnr;
+        String checksum;
+
+        Random generator = new Random();
+        int L1L2, B1B2B3, V1V2;
+
+        L1L2 = generator.nextInt(10);
+        laendercode = String.format("%02d", L1L2);
+
+        B1B2B3 = generator.nextInt(9)+1;
+        bandnr = String.format("%03d", B1B2B3);
+
+        V1V2 = generator.nextInt(9)+1;
+        verlagsnr = String.format("%02d", V1V2);
+
+        checksum = "C";
+
+        // Do not change the following line
+        return laendercode + "-" + bandnr + "-" + verlagsnr + "-" + checksum;
+    }
+
+    /** multiplies i with 2 and subtracts 9 if result is >= 10 */
+    public static int hashOp(int i)
+    {
+        // Do NOT change this method!
+        int doubled = 2 * i;
+        if (doubled >= 10) {
+            doubled = doubled - 9;
+        }
+        return doubled;
+    }
+}
