644a07499308e674988fc163b81986a4060899f8
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2020-10-08 21:08:44 +0200

    finish

--- Serie_2/src/RandomISBN.java
+++ Serie_2/src/RandomISBN.java
@@ -11,7 +11,7 @@ public class RandomISBN
 
     enum ordinals {First, Second, Third;}
 
-    public static void main( String args[] )
+    public static void main( String args[])
     {
         for (ordinals items : ordinals.values()){
         System.out.println(items + " ISBN : " + makeISBN());
