fb331a837fd52f045a201014ff7cb0d617097944
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2020-10-23 01:49:01 +0200

    Done my Son

--- Serie_4/src/problem.java
+++ Serie_4/src/problem.java
@@ -1,2 +1,16 @@
 public class problem {
+
+    public static void main (String[] args){
+
+        int limit = 90;
+        String result = "";
+        if (limit%3 == 0){
+
+            for(int counter = 3; counter != limit ; counter += 3){
+                result = result + counter + " ";}}
+        else {
+            result = "Upper limit is not divisible by 3";}
+
+        System.out.println(result);
+    }
 }
