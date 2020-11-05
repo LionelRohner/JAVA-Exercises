57d3c01fd3a88fe7946be2708c7fd534e9b3fd36
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2020-11-05 21:56:33 +0100

    Serie_5.3
    ALmost done, my sonnn

--- Serie_5_Theory/src/Increment.java
+++ Serie_5_Theory/src/Increment.java
@@ -1,12 +1,25 @@
+import javax.swing.plaf.synth.SynthMenuBarUI;
+
 public class Increment {
 
     public static void main ( String [] args ) {
             Num n1 = new Num (1);
             int n2 = 10;
-            modify (n1 );
-            modify (n2 );
+            modify (n1);
+            modify (n2);
+
+            System.out.println("n1 " + n1);
+            System.out.println("n2 " + n2);
+
+
     }
-    public static void modify (Num num ){ num . setValue (100); }
-    public static void modify (int n) { n =100; }
+    public static void modify (Num num ){ num.setValue (100); }
+    public static void modify (int n)
+    {
+        this.n =100;
+
+    }
+
+
 }
 
--- Serie_5_Theory/src/problem.java
+++ Serie_5_Theory/src/problem.java
@@ -1,11 +1,9 @@
 public class problem {
-
     public static void main (String[] args){
         int [] numbers = {1 ,2 ,3 ,5 ,8 ,13 ,21};
-        for (int i=2; i < numbers.length ; i ++){
+        for (int i=2; i <= numbers.length ; i ++){
             System .out . println ( numbers [i]);
         }
     }
-
 }
 
