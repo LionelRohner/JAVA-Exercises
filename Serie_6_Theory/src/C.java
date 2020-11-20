ec99bbdb04f096865628a43746fb336a633e6b0b
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2020-11-20 12:47:46 +0100

    Cleaned Up the Files and Folders

---
+++ Serie_6_Theory/src/C.java
@@ -0,0 +1,4 @@
+public class C extends B{
+    public C(){ super (); } //constructor; calls constructor from parent B
+    public void foo (){ System .out . println ("C.foo "); } //overrides method foo of A
+}
